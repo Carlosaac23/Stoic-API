72027296153a56000658a7a210cd4a2aa0bdf6dc
Carlos <carlosaac232001@gmail.com>
2025-09-04 17:12:19 -0500

    feat: able to add query to get a random quote

--- app/api/quotes/marcus/route.ts
+++ app/api/quotes/marcus/route.ts
@@ -1,7 +1,17 @@
 import { NextResponse } from 'next/server';
 import quotes from '@/data/quotes.json';
 
-export async function GET() {
-  const marcoQuotes = quotes.filter(quote => quote.author.toLowerCase() === 'marcus aurelius');
-  return NextResponse.json(marcoQuotes);
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const randomParam = searchParams.get('random');
+  const marcoQuotes = quotes.filter(
+    (quote) => quote.author.toLowerCase() === 'marcus aurelius',
+  );
+
+  if (randomParam !== null) {
+    const randomQuote = Math.floor(Math.random() * marcoQuotes.length);
+    return NextResponse.json(marcoQuotes[randomQuote]);
+  } else {
+    return NextResponse.json(marcoQuotes);
+  }
 }
