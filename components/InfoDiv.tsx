1e521ca3c7f21866686449589f9a95fd55479ea6
Carlos <carlosaac232001@gmail.com>
2026-04-17 17:32:35 -0500

    style: reduce font-size inside div

--- components/InfoDiv.tsx
+++ components/InfoDiv.tsx
@@ -2,7 +2,7 @@ import { CircleAlert } from 'lucide-react';
 
 export default function InfoDiv({ text }: { text: string }) {
   return (
-    <div className='mt-16 inline-block rounded-md border border-red-400 bg-red-50 px-6 py-4 text-red-500'>
+    <div className='mt-16 inline-block rounded-md border border-red-400 bg-red-50 px-6 py-4 text-sm text-red-500'>
       <div className='flex items-center gap-2'>
         <CircleAlert />
         {text}
